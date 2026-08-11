25fee20ffa2e1fa7230d5eebc119fac72e75c15d
Yasin Nazari <yasinnazari505@gmail.com>
2026-08-11 03:55:17 +0330

    Added media file for upload images

--- LogixShop/settings.py
+++ LogixShop/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -104,3 +105,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
