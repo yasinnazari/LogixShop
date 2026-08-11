dcdf80dd6d6942a1f9bf0d71e77bde5514268764
Yasin Nazari <yasinnazari505@gmail.com>
2026-08-11 12:27:11 +0330

    Created Customer & Category Model

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 
-class Products(models.Model):
-   ...
+
+class Category(models.Model):
+   title = models.CharField(max_length=30)
+
+   def __str__(self):
+      return self.name
+   
+
+class Customer(models.Model):
+   first_name = models.CharField(max_length=30)
+   last_name = models.CharField(max_length=30)
+   phone_number = models.CharField(max_length=20)
+   email = models.EmailField(max_length=254)
+   password = models.CharField(max_length=20)
+
+   def __str__(self):
+      return self.name
+   
