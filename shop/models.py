ff7d3da7e8c9da0814f8397ca3f51609955c93d7
Yasin Nazari <yasinnazari505@gmail.com>
2026-08-13 23:16:09 +0330

    Calculate product rating in (Views)

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Category(models.Model):
    title = models.CharField(max_length=30)
@@ -18,6 +18,11 @@ class Customer(models.Model):
    password = models.CharField(max_length=20)
 
 
+   def __str__(self):
+      return f"{self.first_name} {self.last_name}"
+
+
+
 class Product(models.Model):
    id = models.AutoField(primary_key=True, unique=True)
    title = models.CharField(max_length=80)
@@ -26,7 +31,11 @@ class Product(models.Model):
    discounted_price = models.DecimalField(max_digits=12, decimal_places=0, default=0)
    category_title = models.ForeignKey(Category, verbose_name="category_title", on_delete=models.CASCADE)
    is_sale = models.BooleanField(default=False)
+   rating = models.DecimalField(max_digits=3, decimal_places=1, default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
    image = models.ImageField(upload_to='upload/product/')
+
+   def __str__(self):
+      return self.title
 
 
 class Invoice(models.Model):
@@ -46,3 +55,7 @@ class Invoice(models.Model):
    address = models.CharField(max_length=300, blank=False)
    created_at = models.DateField(auto_now=False, auto_now_add=True)
    status = models.CharField(max_length=30, choices=InvoiceStatus.choices, default=InvoiceStatus.UNPAID)
+
+   def __str__(self):
+      return "Invoice" + self.id
+
--- shop/views.py
+++ shop/views.py
@@ -6,9 +6,20 @@ def home(request):
    all_products = Product.objects.all()
    all_categories = Category.objects.all()
 
+   for prod in all_products:
+      rate = prod.rating
+      full_star = int(rate)
+      half_star = rate - full_star >= 0.5
+      empty_star = 5 - full_star - int(half_star)
+
+      prod.full_star = full_star
+      prod.half_star = half_star
+      prod.empty_star = empty_star
+
    context = {
       'products': all_products,
       'categories': all_categories
    }
 
    return render(request, "./index.html", context)
+
